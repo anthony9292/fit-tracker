0939b15910076223c288a6e647540628441dcbbe
Anthony Ndegwa <anthon.n24@gmail.com>
2021-07-12 18:43:21 -0400

    finished api-routes

--- routes/api-route.js
+++ routes/api-route.js
@@ -23,7 +23,25 @@ module.exports = function (app) {
  
         })
 
+  app.put("/api/workouts/:id", (req, res) => { 
+      ExerciseModel.findByIdAndUpdate(req.params.id, 
+        { 
+            $push: { exersises: req.body },
+            $inc: { totalDuration: req.body.duration },
+        }, 
+         { new: true, runValidators: true }
+        ).then(workout => { 
+            res.json(workout)
+        });
+  })
     
-
-
+   app.get("/api/workouts/range", (req, res) => { 
+       ExerciseModel.find({}).sort({day: -1}).limit(7)
+       .then(workout => { 
+           res.json(workout.reverse()); 
+       })
+       .catch(err => { 
+           res.json(err);
+       }); 
+   })
 }
